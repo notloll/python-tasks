9994ad64cfdcc5e83b6cfdd05050f013d2bcc592
Mohamed Abdoo <mhndhnm117@gmail.com>
2025-11-03 14:57:18 +0300

    Add files via upload

---
+++ day1Task4.py
@@ -0,0 +1,31 @@
+import random
+
+#task 4
+'''
+print("Welcome to Number Guessing Game!")
+
+attempts = 0
+
+while True:
+    number = random.randint(1, 100)
+
+    
+    attempts=0
+
+    while attempts < 7:
+        guess = int(input("Your guess: ")) 
+        attempts+=1
+
+        if guess ==number :
+            print(f"Correct! You won in {attempts} attempts.")
+            break
+        else:
+            print("wrong guess")
+
+    play_agin= input("Play again? (y/n): ")
+    if play_agin.lower() !='y':
+        break
+        
+'''
+
+#task 5
